954af7357b3057001d649104a2bd1a1ec23b40a4
coderx0805 <coderx0805@gmail.com>
2025-05-31 09:56:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,31 +8,31 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "26399631"))
 API_HASH = getenv("API_HASH", "16b94384632645b5c23ee23ac1b0c8af")
-BOT_PRIVACY = getenv("BOT_PRIVACY", "https://telegra.ph/Privacy-Policy-for-AnieXEricaMusic-10-06")
-BOT_TOKEN = getenv("BOT_TOKEN", "7913305235:AAG_edq-XUnefGP_H4")
+BOT_PRIVACY = getenv("CLONE_BOT", "https://t.me/")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://musicbotxd:musicbotxd@cluster0.6thyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002392274240))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002419831380"))
 
-OWNER_ID = int(getenv("OWNER_ID", 6018803920))
+OWNER_ID = int(getenv("OWNER_ID", "7538572906"))
 
-OWNER = int(getenv("OWNER", 6018803920))
+OWNER = int(getenv("OWNER", "7538572906"))
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-HEROKU_API_KEY = getenv("HEROKU_API_KEY","HRKU-3a48d735-445f-49c4-a6cf-fea438f945ef")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY",None)
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Zfini/Adonis",
+    "https://github.com/coderx0805/DEVABOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = "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"
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MBT_UPDATES")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+EK-lvu4shmBkNmZl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Matlabi_Duniyah")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+MG2n8W4ZpuQwN2Q1")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = '7f92897a59464ddbbf00f06cd6bda7fc'
@@ -40,7 +40,7 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 5242880000))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 5242880000))
 
-STRING1 = getenv("STRING_SESSION","BAFUizgAC0CS3g8afPxNuJ5piWUx02kh_gxZkiEbUCsUsEhzOFOTW4QBYyR6CXnVrODf7WGnGRVT1lizhk4rzhyU5yp950DDhvbqLHosQDIDD8zPJ3hwsfC3Fn2LJ9hBaNtoGCn-heoi9CB-3sgqCUjAZ81M8EviClyy6TV-NRPM6QcdwVlzu2x6O-wmHxNhMAbwaApJxnoVlpIoKEq6vzKWW1-Nr1KeVtivV5gtglIRMjGkd-pkYbsMBHhppFWiAuBEtWvjVqzIZO4RX-2bhm1oG5KYdKDhZQujDRn5Ki0xu5Q1jMW8znESnQhZACMCrHo0OX5dZQqxyl16-VRBLivIGIob5gAAAAGXpY4PAA")
+STRING1 = getenv("STRING_SESSION",  None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
